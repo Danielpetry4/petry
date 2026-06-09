a13f40a4b078cf28d4233ab73079976dc8efff21
Danielpetry4 <danielpetry4@gmail.com>
2026-06-09 16:38:44 -0700

    Fix typo in allow_promotion_codes property

--- routes/payment.js
+++ routes/payment.js
@@ -50,7 +50,7 @@ router.post('/create-checkout-session', async (req, res) => {
         quantity: 1,
       }],
       mode: 'payment',
-      allow_peomotion_codes: true,
+      allow_promotion_codes: true,
       success_url: successUrl,
       cancel_url: cancelUrl,
       customer_email: req.body.customerEmail || undefined,
